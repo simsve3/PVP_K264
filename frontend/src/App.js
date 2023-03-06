5cff46c88cfef5b1ff95ebff319b9c743b0db38e
vilpas <viliusps@gmail.com>
2023-03-06 22:05:40 +0200

    fix: removed unnecessary file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-import Router from "./router.js"
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+import Home from "./Pages/Home.js"
+import TestPage1 from "./Pages/TestPage1.js";
+import TestPage2 from "./Pages/TestPage2.js";
 
 function App() {
   return (
-    <Router />
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/TestPage1" element={<TestPage1 />}/>
+            <Route path="/TestPage2" element={<TestPage2 />}/>
+        </Routes>
+    </Router>
   );
 }
 export default App;
